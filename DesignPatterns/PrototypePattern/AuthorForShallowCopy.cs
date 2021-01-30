e611b52c7c6a5d40de97d043baf41595f36e2a43
Oscar <oscaar_2003@hotmail.com>
2021-01-29 23:05:30 -0300

    PrototypePattern

---
+++ DesignPatterns/PrototypePattern/AuthorForShallowCopy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrototypePattern
+{
+    public class AuthorForShallowCopy : ICloneable
+    {
+        public string Name { get; set; }
+        public string TwitterAccount { get; set; }
+        public string WebSite { get; set; }
+        public Address HomeAddress { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ DesignPatterns/Tests/CreationalPatterns/Prototype/PrototypeTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using PrototypePattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests.CreationalPatterns.Prototype
+{
+    public class PrototypeTest
+    {
+        [Fact]
+        public void AuthorTest()
+        {
+            AuthorForShallowCopy author = new AuthorForShallowCopy()
+            { 
+                Name = "Oscar Cama",
+                TwitterAccount = "http://twitter.com/OscarCama",
+                HomeAddress = new Address()
+                {
+                    City = "Cercado de Lima",
+                    State = "Lima"
+                }
+            };
+
+            AuthorForShallowCopy cloneAuthor = (AuthorForShallowCopy)author.Clone();
+
+            author.Name.Should().Equals(cloneAuthor.Name);
+
+            cloneAuthor.Name = "Jose";
+
+            cloneAuthor.Name.Should().NotBe(author.Name);
+        }
+    }
+}
